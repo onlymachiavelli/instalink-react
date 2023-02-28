228fa65fe34b091ae22519781a24329319318ed6
Alaa Barka <tmakaveli643@gmail.com>
2023-02-28 14:47:21 +0100

    tailwnind files

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -2,14 +2,11 @@ import React, {} from 'react'
 
 
 const Header=  () =>{
-
     return (
-        <header className='w-full h-auto bg-[071e40]'>
-            hello wrold
+        <header className='w-full h-auto bg-[#071e40] p-2'>
+            <b className='text-white flex gap-4'><i className="fa fa-map-marker" aria-hidden="true"></i> <p>Only in Tunisia </p></b>
         </header>
     )
-
-
 }
 
 
