d76e3e592676e223feae159394510ff386a03a5d
Alaa Barka <tmakaveli643@gmail.com>
2023-02-28 23:37:49 +0100

    some navbar

---
+++ src/components/navbar.tsx
@@ -0,0 +1,17 @@
+import React, {} from 'react'
+
+
+const Navbar = () =>{
+
+    return (
+        <nav className='w-full h-auto bg-none'>
+            <div>
+                <a href="">
+                    <img src="./../assets/logo.png" />
+                </a>
+            </div>
+        </nav>
+    ) 
+}
+
+export default Navbar
--- src/components/sld.tsx
+++ src/components/sld.tsx
@@ -1,10 +1,12 @@
 import React, {} from 'react'
-
+import Navbar from './navbar'
 
 const SLD = () =>{
     return (
         <div className='w-full h-screen bg-company bg-cover bg-norepeat'>
-            <div className='w-full h-full bg-[#000000a1]'></div>
+            <div className='w-full h-full bg-[#000000a1]'>
+                <Navbar/>
+            </div>
         </div>
     )
 }
