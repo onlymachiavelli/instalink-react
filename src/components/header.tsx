4c67f49f7126f44c1336584ff63dac5eb4176ebc
Alaa Barka <tmakaveli643@gmail.com>
2023-02-28 14:25:11 +0100

    preparing tailwind and some of that header

---
+++ src/components/header.tsx
@@ -0,0 +1,16 @@
+import React, {} from 'react'
+
+
+const Header=  () =>{
+
+    return (
+        <header className='w-full h-auto bg-[071e40]'>
+            hello wrold
+        </header>
+    )
+
+
+}
+
+
+export default Header
