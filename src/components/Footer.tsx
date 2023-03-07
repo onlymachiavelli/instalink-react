dd559e9c14f13521173dab5bd07e2dd1c9928c57
Alaa Barka <tmakaveli643@gmail.com>
2023-03-07 16:01:26 +0100

    footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,8 +3,8 @@ import React, {  } from 'react'
 const Footer = () =>{
 
     return (
-        <footer className='w-full h-auto bg-[#111] py-10'>
-
+        <footer className='w-full h-auto bg-[#111] pt-10 p-5 text-center'>
+            <p className='block text-white pb-5 '>Instalink &copy; All Right Reserved.</p>
 
         </footer>
         ) 
