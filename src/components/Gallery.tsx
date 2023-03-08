b6cb0d8360e60f0d0c55aec7dd5cf2f79650c2ce
Alaa Barka <tmakaveli643@gmail.com>
2023-03-08 11:53:39 +0100

    slide images

--- src/components/Gallery.tsx
+++ src/components/Gallery.tsx
@@ -1,19 +1,45 @@
 import React, {} from 'react'
 import useImages from './Images'
+import 'react-slideshow-image/dist/styles.css'
+
+import {Fade, Zoom , } from 'react-slideshow-image'
 const Gallery = () =>{
     const {images, setImg} = useImages()
+    const spanStyle = {
+        padding: '20px',
+        background: '#efefef',
+        color: '#000000'
+      }
+      
+      const divStyle = {
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundSize: 'cover',
+        height: '400px'
+      }
     return (
-        <div className='w-3/4 h-auto grid grid-cols-3 overflow-hidden m-auto'>
-            {
+        <div className='w-full h-screen overflow-hidden '>
+            <Zoom scale={1.4} indicators={true} autoplay={true}>
+            {images.map((each: any, index :any) => (
+                <div key={index} style={{ width: "100%" }}>
+                    <img style={{ objectFit: "cover", width: "100%", }} alt="Slide Image" src={each}  className="h-screen"/>
+                </div>
+            ))}
+        </Zoom>
+        </div>
+    )
+}
+
+export default Gallery
+
+/*
+
+    {
                 images.map((img:any, index : any )=>{
                     return (
                         <img  width="400" height={"400"} src={img} className="object-cover"/>
                     )
                 })
             }
-
-        </div>  
-    )
-}
-
-export default Gallery
+*/
